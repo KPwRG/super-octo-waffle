80c4666596f4446dc0832ddfb69843022ff29f35
KPwRG <mjhamersma@gmail.com>
2020-04-07 15:47:55 -0500

    4/7 PM Update

---
+++ coding_dojo_python/python_stack/python/OOP/userAssignment.py
@@ -0,0 +1,64 @@
+class User:		
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+    
+    def make_deposit(self, amount):	
+    	self.account_balance += amount	
+
+    def make_withdrawal(self, amount):
+        if amount > self.account_balance:
+            print(f"Insufficient funds. Amount available for withdrawal is {self.account_balance}.")
+        else:
+            self.account_balance -= amount
+
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: ${self.account_balance}.")
+
+    # def transfer_money(self, other_user, amount):
+    #     self.account_balance -= amount
+
+
+# guido = User("Guido van Rossum", "guido@python.com")
+# monty = User("Monty Python", "monty@python.com")
+michael = User("Michael Myers", "themask@python.com")
+walter = User("Walter White", "methman@python.com")
+paul = User("Paul Lambert", "meatsauce@python.com")
+
+
+
+# print(guido.name)	# output: Guido van Rossum
+# print(monty.name)	# output: Monty Python
+
+
+# guido.make_deposit(100)
+# guido.make_deposit(200)
+# monty.make_deposit(50)
+# print(guido.account_balance)	# output: 300
+# print(monty.account_balance)	# output: 50
+
+# guido.make_withdrawal(75)
+# monty.make_withdrawal(25)
+
+# guido.display_user_balance()
+# monty.display_user_balance()
+
+michael.make_deposit(200)
+michael.make_deposit(15)
+michael.make_deposit(45)
+michael.make_withdrawal(50)
+michael.display_user_balance()
+
+walter.make_deposit(100)
+walter.make_deposit(100)
+walter.make_withdrawal(25)
+walter.make_withdrawal(25)
+walter.display_user_balance()
+
+paul.make_deposit(500)
+paul.make_withdrawal(50)
+paul.make_withdrawal(25)
+paul.make_withdrawal(250)
+paul.display_user_balance()
+
--- stooperPigProblems/sppPython/test.py
+++ stooperPigProblems/sppPython/test.py
@@ -1,1 +1,41 @@
-print("mj")
+# If number of opening does not equal to number of closing parenthesis
+# If closing parenthesis does not have corresponding opening parenthesis before it
+
+def parensValid(stringInput):
+    openParensCount = 0
+    closeParenCount = 0
+    for i in range(0, len(stringInput), 1):
+        if stringInput[i] == "(":
+            openParensCount += 1
+        elif stringInput[i] == ")":
+            closeParenCount += 1
+
+        if closeParenCount > openParensCount:
+            return False
+
+    if openParensCount != closeParenCount:
+        return False
+    else:
+        return True
+
+
+# def parensValid(stringInput):
+#     openParensCount = 0
+#     closeParensCount = 0
+#     for i in range(0, len(stringInput), 1):
+#         if stringInput[i] == "(":
+#             openParensCount +=1
+#         elif stringInput[i] == ")":
+#             closeParensCount += 1
+#         if closeParensCount > openParensCount:
+#             return False
+        
+
+#     if openParensCount != closeParensCount:
+#         return False
+#     else:
+#         return True
+
+
+
+print(parensValid("((()))"))
