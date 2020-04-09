5f936a3cb3ed684e157985c7e7ebfddcfb2182d9
KPwRG <mjhamersma@gmail.com>
2020-04-09 16:40:37 -0500

    4/9 PM Update

--- coding_dojo_python/python_stack/python/fundamentals/fuctionsIntermediateII.py
+++ coding_dojo_python/python_stack/python/fundamentals/fuctionsIntermediateII.py
@@ -5,19 +5,30 @@
 
 # Update Values in Dictionaries and Lists
 x = [[5, 2, 3], [10, 8, 9]]
-x[1,0]
+x[1] [0] = 15
+print(x)
 
 students = [
     {'first_name':  'Michael', 'last_name': 'Jordan'},
     {'first_name': 'John', 'last_name': 'Rosales'}
 ]
+
+students[0]['last_name'] = 'Bryant'
+print(students)
+
 sports_directory = {
     'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
     'soccer': ['Messi', 'Ronaldo', 'Rooney']
 }
-z = [{'x': 10, 'y': 20}]
 
-print(x)
+sports_directory["soccer"][0] = "Andres"
+print(sports_directory)
+
+
+z = [{'x': 10, 'y': 20}]
+z[0]["y"]=30
+print(z)
+
 
 # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 # Change the last_name of the first student from 'Jordan' to 'Bryant'
@@ -34,14 +45,19 @@ students = [
     {'first_name': 'Mark', 'last_name': 'Guillen'},
     {'first_name': 'KB', 'last_name': 'Tonel'}
 ]
-iterateDictionary(students)
+# iterateDictionary(students)
 # should output: (it's okay if each key-value pair ends up on 2 separate lines;
 # bonus to get them to appear exactly as below!)
-first_name - Michael, last_name - Jordan
-first_name - John, last_name - Rosales
-first_name - Mark, last_name - Guillen
-first_name - KB, last_name - Tonel
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
 
+def iterateDictionary(listInput):
+    for key in listInput:
+        print(key)
+
+iterateDictionary(students)
 
 # Get Values From a List of Dictionaries
 # Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
@@ -69,7 +85,7 @@ dojo = {
 }
 
 def printInfo(dictInput):
-    for key, val in dojo.items():
+    for key, val in dictInput.items():
         print(f"{len(val)} {key.upper()}")
         for item in val:
             print(item)
--- stooperPigProblems/sppPython/test.py
+++ stooperPigProblems/sppPython/test.py
@@ -85,3 +85,30 @@ print(isPalidrome("potato"))
 #         return False
 #     else:
 #         return True
+
+
+
+
+# Book Index
+
+y = [4,5,6,12,14,18,19,20,21,22,56]
+
+# desired result = "4-6,12,14,18-22,56"
+
+
+def bookIndex(listInput):
+    newString = ""
+    seqCounter = 0
+    for i in range(0, len(listInput)-1, 1):
+        if listInput[i+1]  == listInput[i] + 1:
+            if seqCounter == 0:
+                newString += f"{listInput[i]} - "
+            seqCounter += 1
+        
+        else:
+            newString += f"{listInput[i]}, "
+            seqCounter = 0
+    newString += f"{listInput[i+1]}"
+    return newString
+
+print(bookIndex(y))         
