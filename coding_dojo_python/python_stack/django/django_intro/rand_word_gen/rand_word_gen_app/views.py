cf5a3a49a41cbbe093d2a104f49aa5d13e14729d
MJ Hamersma <mjhamersma@gmail.com>
2020-04-15 21:20:56 -0500

    4/15 PM Update

--- coding_dojo_python/python_stack/django/django_intro/rand_word_gen/rand_word_gen_app/views.py
+++ coding_dojo_python/python_stack/django/django_intro/rand_word_gen/rand_word_gen_app/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
 
 def index(request):
-    return render(request, "index.html")
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
+    else:
+        request.session['counter'] += 1
+
+    if request.session['counter'] <= 1:
+        random_word = ""
+    else:
+        random_word = get_random_string(length=14)
+    context = {
+        'randomWord' : random_word
+    }
+    return render(request, "randomWord.html", context)
+
+def generate(request):
+    return redirect("/")
+
+def reset(request):
+    request.session.clear()
+    return redirect("/")
