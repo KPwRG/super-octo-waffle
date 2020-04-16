bb5770007f69503ad77fef312db4d9f964e8b40a
MJ Hamersma <mjhamersma@gmail.com>
2020-04-16 15:30:35 -0500

    4/16 PM Update

---
+++ coding_dojo_python/python_stack/django/django_orm/strings/stringsApp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+    
+class Guitars(models.Model):
+    brand = models.CharField(max_length=45)
+    model = models.TextField()
+    production_year = models.DateTimeField()
+    number_of_strings = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
