dcb045b2a42e629c72db1cd5b67b6c946045855f
MJ Hamersma <mjhamersma@gmail.com>
2020-04-19 13:20:06 -0500

    4/19 AM Update

--- coding_dojo_python/python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/models.py
+++ coding_dojo_python/python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __repr__(self):
+        return f"<Dojos objects: {self.name} ({self.id})>"
+
+class Ninjas(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo_id = models.ForeignKey(Dojos, related_name="ninjas", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __repr__(self):
+        return f"<Ninjas objects: (({self.first_name} + {self.last_name}) ({self.id}))>"
+
