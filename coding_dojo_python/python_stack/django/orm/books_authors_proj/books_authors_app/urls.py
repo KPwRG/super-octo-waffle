f9dbeafa27bf476d3026b3eb974739973c683796
MJ Hamersma <mjhamersma@gmail.com>
2020-04-24 18:18:05 -0500

    4/24 PM Update

--- coding_dojo_python/python_stack/django/orm/books_authors_proj/books_authors_app/urls.py
+++ coding_dojo_python/python_stack/django/orm/books_authors_proj/books_authors_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('createBook', views.createBook),	   
+    path('createBook', views.createBook),
+    path('books/<bookId>', views.showBook),
+    path('addAuthorToBook/<bookId>', views.addAuthorToBook),	   
 ]
--- coding_dojo_python/python_stack/django/orm/books_authors_proj/books_authors_app/views.py
+++ coding_dojo_python/python_stack/django/orm/books_authors_proj/books_authors_app/views.py
@@ -11,3 +11,18 @@ def index(request):
 def createBook(request):
     newBook = Books.objects.create(title = request.POST['bookTitle'], desc = request.POST['description'])
     return redirect('/')
+
+def showBook(request, bookId):
+    bookToShow = Books.objects.get(id=bookId)
+    context = {
+        'bookInfo': bookToShow,
+        'bookAuthors': Authors.objects.filter(books=bookId),
+        'allAuthors': Authors.objects.all()
+    }
+    return render(request, 'bookId.html', context)
+
+def addAuthorToBook(request, bookId):
+    book = Books.objects.get(id=bookId)
+    authorAdded = Authors.objects.get(id= request.POST['authorId'])
+    authorAdded.books.add(book)
+    return redirect('/books/<bookId>')
