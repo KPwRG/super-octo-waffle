3f2008bf42c6099f9ef53b04f699aa0521ab61b8
MJ Hamersma <mjhamersma@gmail.com>
2020-04-26 14:53:30 -0500

    4/26 PM Update

--- coding_dojo_python/python_stack/django/orm/books_authors_proj/books_authors_app/urls.py
+++ coding_dojo_python/python_stack/django/orm/books_authors_proj/books_authors_app/urls.py
@@ -3,6 +3,10 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('createBook', views.createBook),
-    path('books/<bookId>', views.showBook),
-    path('addAuthorToBook/<bookId>', views.addAuthorToBook),	   
+    path('books/<int:bookId>', views.showBook),
+    path('addAuthorToBook/<int:bookId>', views.addAuthorToBook),
+    path('authors', views.authors),
+    path('createAuthor', views.createAuthor),
+    path('authors/<int:authorId>', views.showAuthor),
+    path('addBookToAuthor/<int:authorId>', views.addBookToAuthor),	   
 ]
--- coding_dojo_python/python_stack/django/orm/books_authors_proj/books_authors_app/views.py
+++ coding_dojo_python/python_stack/django/orm/books_authors_proj/books_authors_app/views.py
@@ -8,9 +8,20 @@ def index(request):
     }
     return render(request,"book.html", context)
 
+def authors(request):
+    allAuthors = Authors.objects.all()
+    context = {
+        'authors': allAuthors
+    }
+    return render(request, "author.html", context)
+
 def createBook(request):
     newBook = Books.objects.create(title = request.POST['bookTitle'], desc = request.POST['description'])
     return redirect('/')
+
+def createAuthor(request):
+    newAuthor = Authors.objects.create(first_name=request.POST['firstName'], last_name=request.POST['lastName'], notes=request.POST['notes'])
+    return redirect('/authors')
 
 def showBook(request, bookId):
     bookToShow = Books.objects.get(id=bookId)
@@ -21,8 +32,23 @@ def showBook(request, bookId):
     }
     return render(request, 'bookId.html', context)
 
+def showAuthor(request, authorId):
+    authorToShow = Authors.objects.get(id=authorId)
+    context = {
+        'authorInfo': authorToShow,
+        'authorBooks': Books.objects.filter(authors=authorId),
+        'allBooks': Books.objects.all()
+    }
+    return render(request, 'authorId.html', context)
+
 def addAuthorToBook(request, bookId):
     book = Books.objects.get(id=bookId)
     authorAdded = Authors.objects.get(id= request.POST['authorId'])
     authorAdded.books.add(book)
-    return redirect('/books/<bookId>')
+    return redirect(f'/books/'+str(bookId))
+
+def addBookToAuthor(request, authorId):
+    author = Authors.objects.get(id=authorId)
+    bookAdded = Books.objects.get(id=request.POST['bookId'])
+    bookAdded.authors.add(author)
+    return redirect(f'/authors/'+str(authorId))
